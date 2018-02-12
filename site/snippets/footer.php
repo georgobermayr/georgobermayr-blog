ca1835673c39c411a6e2ff6e4bae0745e5f21590
Georg Obermayr <g.obermayr@adverma.de>
2018-02-12 20:36:31 +0100

    Basic styling defintions

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,2 +1,4 @@
+  <?php echo js('assets/js/main.min.js') ?>
+
 </body>
 </html>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <title>
+    <?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>
+  </title>
+
+  <?php echo css('assets/css/main.min.css') ?>
 
 </head>
-<body>
+
+<body class="<?php echo 'body body--' . str_replace('.', '-', $page->template()) ?>">
