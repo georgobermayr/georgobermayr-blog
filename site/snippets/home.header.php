a3b0c372946aa26dae7e8640f0a62fc61c1f2124
Georg Obermayr <g.obermayr@adverma.de>
2018-02-25 13:12:16 +0100

    Home header: content, design, responsive

---
+++ site/snippets/home.header.php
@@ -0,0 +1,11 @@
+<div class="home__header">
+
+  <h1>
+    <?php echo $page->intro() ?>
+  </h1>
+
+  <img
+    src="<?php echo url('/assets/images/georg-obermayr.jpg') ?>"
+    alt="Georg Obermayr">
+
+</div>
---
+++ site/templates/home.php
@@ -0,0 +1,23 @@
+<?php snippet('metadata') ?>
+
+<header>
+
+  <?php snippet('header') ?>
+
+  <?php snippet('home.header') ?>
+
+</header>
+
+<main>
+
+
+
+</main>
+
+<footer>
+
+  <?php snippet('footer') ?>
+
+</footer>
+
+<?php snippet('end') ?>
