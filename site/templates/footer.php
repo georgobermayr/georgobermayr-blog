08c8b89954bb392885ef47fa6e0a469d771c2d0f
Georg Obermayr <g.obermayr@adverma.de>
2018-03-03 18:43:56 +0100

    Adding legal pages

---
+++ site/templates/footer.php
@@ -0,0 +1,21 @@
+<?php snippet('metadata') ?>
+
+<header>
+
+  <?php snippet('header', array('color' => '')) ?>
+
+</header>
+
+<main class="legal">
+
+  <?php echo $page->text() ?>
+
+</main>
+
+<footer>
+
+  <?php snippet('footer') ?>
+
+</footer>
+
+<?php snippet('end') ?>
