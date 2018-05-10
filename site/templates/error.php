b41c66ec2e67122385a032abb287b3a5ad15e7d3
Georg Obermayr <g.obermayr@adverma.de>
2018-05-10 10:32:35 +0200

    404 error page

---
+++ site/templates/error.php
@@ -0,0 +1,37 @@
+<?php snippet('metadata') ?>
+
+<header>
+
+  <?php snippet('header', array('color' => '')) ?>
+
+</header>
+
+<main class="legal">
+
+  <h1>
+    Page not found ☹️
+  </h1>
+
+  <h2>
+    Sorry, the page you requested could not be found.
+  </h2>
+
+  <p>
+    During the website relaunch, I removed a lot of old and outdated articles. So, there is a good chance the article you requested simply doesn't exist anymore. Sorry for that!
+  </p>
+  <p>
+    There is a good chance for newer and better articles about the topic you wanted to read about somewhere in the internet. If you are interested in my articles about QuarkXPress and Color Management I can strongly recommend Detlev Hageman’s  Site (in German): <a href="http://www.xpress-upgrade.com" target="_blank">xpress-upgrade.com</a>.
+  </p>
+  <p>
+    In any case, I hope you can find some other interesting content on my site: I think there is plenty of read worthy stuff about Machine Learning, Content Strategy, Multichannel Technology and Agile Publishing. Enjoy!
+  </p>
+
+</main>
+
+<footer>
+
+  <?php snippet('footer') ?>
+
+</footer>
+
+<?php snippet('end') ?>
