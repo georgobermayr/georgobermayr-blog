a8159b8afb37334d8caaa310519b40bddf0a6450
Georg Obermayr <g.obermayr@adverma.de>
2018-05-10 17:48:56 +0200

    Update to user file

--- site/accounts/georgobermayr.php
+++ site/accounts/georgobermayr.php
@@ -7,21 +7,17 @@ email: g.obermayr@adverma.de
 language: en
 role: admin
 history:
-  - home
-  - >
-    articles/are-we-on-the-right-track-with-todays-digital-publishing
-  - >
-    articles/setting-the-stage-for-agile-publishing-with-a-war-room
-  - >
-    articles/building-multichannel-experiences-with-the-content-topic-wheel
-  - >
-    articles/about-layout-applications-for-content-first
+  - privacy
+  - articles/the-illustrator-moment-for-machine-learning
+  - articles/everything-is-a-workshop
+  - articles/agile-is-not-a-religion
+  - articles/image-cropping-in-the-machine-learning-age
 firstname: Georg
 lastname: Obermayr
 biography: |
-  I'm one of those guys in the media production and publishing scene, that is often labeled as a thought leader. But I'm a practitioner. Day in an day out I work as Head of Crossmedia Production in an advertising agency. I'm hands on creating content infrastructures and designing websites, apps and social media stuff that are driven by these infrastrucutures. This it what grounds me. And it is this daily business work that helps me identifying the trends and emerging topics of our field. With that kind of real world knowledge, I'm an active participant in bringing our industry forward: I write a lot about agile publishing, digital publishing, development, and media production, not just here but also in well know magazines and journals. I'm a keynote speaker at conferences and do a lot of trainings and consulting work. Since I also know a thing or two about print publishing I'm involved in developing industry guidelines for PDFX-ready. I co-authored the book "Agile Publishing", still the 400 pages reference work on how agile processes move user experience and storytelling in the spotlight of todays multichannel world. I'm living at the intersection of design, content, technology, marketing and consulting. How hypes can be moved into practical use is what drives me every day.
+  I’m one of those guys in the media production and publishing scene, that is often labeled as a thought leader. But I’m a practitioner. Day in an day out I work as Head of Crossmedia Production in an advertising agency. I’m hands on creating content infrastructures and designing websites, apps and social media stuff that are driven by these infrastrucutures. This it what grounds me. And it is this daily business work that helps me identifying the trends and emerging topics of our field. With that kind of real world knowledge, I’m an active participant in bringing our industry forward: I write a lot about agile publishing, digital publishing, development, and media production, not just here but also in well know magazines and journals. I’m a keynote speaker at conferences and do a lot of trainings and consulting work. Since I’m originally a print person, I was involved in developing industry guidelines for PDFX-ready. I co-authored the book “Agile Publishing”, still the 400 pages reference work on how agile processes move user experience and storytelling in the spotlight of todays multichannel world. I’m living at the intersection of design, content, technology and marketing. How hypes can be moved into practical use is what drives me every day.
   <br>
-  <a href="https://www.xing.com/profile/Georg_Obermayr" target="_blank">www.xing.com/profile/Georg_Obermayr</a><br>
-  <a href="https://www.linkedin.com/in/georgobermayr" target="_blank">www.linkedin.com/in/georgobermayr</a><br>
-  <a href="https://www.twitter.com/georgobermayr" target="_blank">www.twitter.com/georgobermayr</a><br>
-  <a href="http://goo.gl/LurZMl" target="_blank">Buy the book "Agile Publishing" on Amazon</a>
+  <a href="https://www.xing.com/profile/Georg_Obermayr" target="_blank" rel="noopener">www.xing.com/profile/Georg_Obermayr</a><br>
+  <a href="https://www.linkedin.com/in/georgobermayr" target="_blank" rel="noopener">www.linkedin.com/in/georgobermayr</a><br>
+  <a href="https://www.twitter.com/georgobermayr" target="_blank" rel="noopener">www.twitter.com/georgobermayr</a><br>
+  <a href="http://goo.gl/LurZMl" target="_blank" rel="noopener">Buy the book "Agile Publishing" on Amazon</a>
